76977da570cc3be1b7a2b836e8110a98d9cc170c
Tristan <tristan141parrish@gmail.com>
2025-04-26 18:31:42 -0400

    init commit

---
+++ tristanparrish/src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <h1 className="bg-neutral-900 h-screen flex items-center justify-center">
+      Placeholder - Tristan Parrish
+    </h1>
+  );
+}
+
+export default App;
