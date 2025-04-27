e7330dc23eb216c41a9fc80bb5879f8d1e0e3391
Tristan <tristan141parrish@gmail.com>
2025-04-27 17:09:23 -0400

    Import welcome page into content

--- tristanparrish/src/components/Content.tsx
+++ tristanparrish/src/components/Content.tsx
@@ -1,11 +1,11 @@
 import Education from "../sections/content_sections/Education";
 import Experience from "../sections/content_sections/Experience";
 import AboutMe from "../sections/content_sections/AboutMe";
-import Portfolio from "../sections/content_sections/Portfolio";
 import Certifications from "../sections/content_sections/Certifications";
+import Welcome from "../sections/content_sections/Welcome";
 
 // Define shared class names
-const titleClassName = "font-bold text-4xl md:text-6xl mb-5";
+const titleClassName = "font-bold text-4xl md:text-5xl mb-5";
 
 export default function Content({ selection }: { selection: string }) {
   return (
@@ -16,10 +16,8 @@ export default function Content({ selection }: { selection: string }) {
       {selection === "WorkExperience" && (
         <Experience titleClassName={titleClassName} />
       )}
-      {selection === "Home" && <AboutMe titleClassName={titleClassName} />}
-      {selection === "Portfolio" && (
-        <Portfolio titleClassName={titleClassName} />
-      )}
+      {selection === "Home" && <Welcome titleClassName={titleClassName} />}
+      {selection === "AboutMe" && <AboutMe titleClassName={titleClassName} />}
       {selection === "Certifications" && (
         <Certifications titleClassName={titleClassName} />
       )}
