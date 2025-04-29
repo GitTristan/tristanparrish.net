d21ad342c08e2d552a4ddf2a2652eb1d128d9195
Tristan <tristan141parrish@gmail.com>
2025-04-28 21:15:13 -0400

    Update certifications page

--- src/sections/content_sections/Certifications.tsx
+++ src/sections/content_sections/Certifications.tsx
@@ -1,3 +1,6 @@
+import { CompTIAIcon } from "../../assets/icons/myicons";
+import TestOutLogo from "../../assets/icons/testout.webp";
+
 interface CertificationsProps {
   titleClassName?: string;
 }
@@ -8,36 +11,23 @@ export default function Certifications({
   return (
     <div className="py-10 xl:py-0 flex flex-col items-center justify-center w-full gap-5">
       <h1 className={titleClassName}>Certifications</h1>
-      <div className="grid grid-cols-3 hover:bg-white/10 bg-white/5 duration-300 rounded-md p-5 gap-5">
-        <div className="opacity-70">
-          <p>Issue Date — Oct 2023</p>
-          <p>Expiration Date — Oct 2026</p>
+      <div className="grid grid-cols-2 gap-5">
+        <div className="flex flex-col hover:bg-white/10 bg-white/5 duration-300 rounded-md p-5 gap-2 items-center">
+          <CompTIAIcon className="h-8" />
+          <h2 className="text-2xl font-bold">A+</h2>
+          <div className="opacity-70 text-sm">
+            <p>Issue Date — Oct 2023</p>
+            <p>Expiration Date — Oct 2026</p>
+            <p>ID: QLS9E2H62MQ117KB</p>
+          </div>
         </div>
-        <div className="col-span-2">
-          <h2>CompTIA A+</h2>
-          <p className="opacity-70 text-sm">
-            Pursuing a Bachelor's degree focused on cybersecurity, networking,
-            and information systems security. Building technical expertise in
-            areas such as risk management, ethical hacking, network defense, and
-            cloud security while gaining hands-on experience with modern
-            security tools and practices.
-          </p>
-          <p>ID: QLS9E2H62MQ117KB</p>
-        </div>
-      </div>
-      <div className="grid grid-cols-3 hover:bg-white/10 bg-white/5 duration-300 rounded-md p-5 gap-5">
-        <div className="opacity-70">
-          <p>Issue Date — Feb 2023</p>
-        </div>
-        <div className="col-span-2">
-          <h2>TestOut Security Pro</h2>
-          <p className="opacity-70 text-sm">
-            Pursuing a Bachelor's degree focused on cybersecurity, networking,
-            and information systems security. Building technical expertise in
-            areas such as risk management, ethical hacking, network defense, and
-            cloud security while gaining hands-on experience with modern
-            security tools and practices.
-          </p>
+        <div className="flex flex-col hover:bg-white/10 bg-white/5 duration-300 rounded-md p-5 gap-2 items-center">
+          <img src={TestOutLogo} alt="Test Out Logo" className="h-8" />
+          <h2 className="text-2xl font-bold">Security Pro</h2>
+          <div className="opacity-70 text-sm">
+            <p>Issue Date — Feb 2023</p>
+            <p>ID: 6-1C6-VV969X</p>
+          </div>
         </div>
       </div>
     </div>
